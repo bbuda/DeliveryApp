ef0a1d521a26c7bdf994b3037ed010e0f871d63b
Alexei Vashkevich <aleksvkv95@gmail.com>
2023-07-05 15:22:55 +0300

    add view initilization hello controller

--- src/main/java/ru/bbuda/controller/HelloController.java
+++ src/main/java/ru/bbuda/controller/HelloController.java
@@ -1,14 +1,33 @@
 package ru.bbuda.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ResourceBundle resources;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private URL location;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button registrationButton;
+
+    @FXML
+    private Button signButton;
+
+    @FXML
+    void initialize() {
+
     }
 }
