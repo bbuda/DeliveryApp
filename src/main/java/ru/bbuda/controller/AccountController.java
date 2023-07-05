857dbdc37bd90d52e31163f6a0aaf6c7569efde5
Alexei Vashkevich <aleksvkv95@gmail.com>
2023-07-05 17:43:40 +0300

    add personal-account view and controller

---
+++ src/main/java/ru/bbuda/controller/AccountController.java
@@ -0,0 +1,20 @@
+package ru.bbuda.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class AccountController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
