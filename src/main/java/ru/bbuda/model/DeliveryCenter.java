d5638cdde39316d0516881c4b6a9094c5bcee1e5
Alexei Vashkevich <aleksvkv95@gmail.com>
2023-07-06 05:33:30 +0300

    add couriers

--- src/main/java/ru/bbuda/model/DeliveryCenter.java
+++ src/main/java/ru/bbuda/model/DeliveryCenter.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @Entity
@@ -17,4 +19,6 @@ public class DeliveryCenter {
     @JoinColumn(name = "address_name", referencedColumnName = "name", nullable = false)
     private Address address;
 
+    @OneToMany(mappedBy = "deliveryCenter", cascade = CascadeType.ALL)
+    private List<Courier> couriers;
 }
